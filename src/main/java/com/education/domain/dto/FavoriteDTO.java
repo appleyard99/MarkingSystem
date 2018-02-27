6b344bf36c32c357bdb38a6c7c673391c296d9c8
wuanran <wuanran@meituan.com>
2018-02-27 18:40:57 +0800

    update favorite

--- src/main/java/com/education/domain/dto/FavoriteDTO.java
+++ src/main/java/com/education/domain/dto/FavoriteDTO.java
@@ -1,13 +1,19 @@
 package com.education.domain.dto;
 
 import com.education.domain.vo.FavoriteVo;
-import lombok.Getter;
-import lombok.Setter;
+import com.education.domain.vo.QbKnowledgeVo;
 
 import java.util.List;
 
 public class FavoriteDTO extends FavoriteVo {
-    @Getter
-    @Setter
-    private List knowinfo;
+
+    private List<QbKnowledgeVo> knowinfo;
+
+    public List<QbKnowledgeVo> getKnowinfo() {
+        return knowinfo;
+    }
+
+    public void setKnowinfo(List<QbKnowledgeVo> knowinfo) {
+        this.knowinfo = knowinfo;
+    }
 }
